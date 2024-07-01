2055b5435bef147249cf745f70368ee68a9863f6
Anhvdse170059 <anhvdse170059@fpt.edu.vn>
2024-07-01 21:31:07 +0700

    Demo payment with Paypal method

---
+++ Payment-Paypal/src/main/java/swp391/payment/entity/Product.java
@@ -0,0 +1,60 @@
+package swp391.payment.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "Products")
+public class Product {
+    @Id
+    @Column(name = "productID")
+    private String productID;
+
+    @Column(name = "productName")
+    private String productName;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "description")
+    private String description;
+
+    // getters and setters
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
